1c0e791cb25a927adfc5b549d73fad2eb1ea823d
xaxage <xaxage@gmail.com>
2022-01-08 06:37:16 -0800

    Adding base entity. Closes #11

--- pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.xaxage.xaxagepetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.xaxage.xaxagepetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
