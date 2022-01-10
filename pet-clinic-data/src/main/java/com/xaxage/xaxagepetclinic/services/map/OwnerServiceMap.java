5fd9f8436205ce3a0233c90bc78e17ff12c3efea
xaxage <xaxage@gmail.com>
2022-01-10 07:07:30 -0800

    Implement map-based services. Closes #7

---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.xaxage.xaxagepetclinic.services.map;
+
+import com.xaxage.xaxagepetclinic.model.Owner;
+import com.xaxage.xaxagepetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.xaxage.xaxagepetclinic.services.map;
+
+import com.xaxage.xaxagepetclinic.model.Vet;
+import com.xaxage.xaxagepetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
