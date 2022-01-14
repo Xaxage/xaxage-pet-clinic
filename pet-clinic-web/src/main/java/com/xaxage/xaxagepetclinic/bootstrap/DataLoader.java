f64c6d1a5e1f19a9a56a27d18e856e323c34e03a
xaxage <xaxage@gmail.com>
2022-01-14 22:16:38 +0400

    Refactor map implementation to abstract ID generation. Closes #24

--- pet-clinic-web/src/main/java/com/xaxage/xaxagepetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/xaxage/xaxagepetclinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Myers");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Shreki");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Winchester");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Pinkman");
 
