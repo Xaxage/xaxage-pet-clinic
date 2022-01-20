498f26a2337324b3620a2a337596ee0732919269
xaxage <xaxage@gmail.com>
2022-01-21 02:04:15 +0400

    Add PetTypes to bootstrap. Closes #33

--- pet-clinic-web/src/main/java/com/xaxage/xaxagepetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/xaxage/xaxagepetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.xaxage.xaxagepetclinic.bootstrap;
 
 import com.xaxage.xaxagepetclinic.model.Owner;
+import com.xaxage.xaxagepetclinic.model.PetType;
 import com.xaxage.xaxagepetclinic.model.Vet;
 import com.xaxage.xaxagepetclinic.services.OwnerService;
+import com.xaxage.xaxagepetclinic.services.PetTypeService;
 import com.xaxage.xaxagepetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,16 +16,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
     @Autowired//Kinda like it being here
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Myers");
