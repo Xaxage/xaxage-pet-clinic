e969e8d3aa6decc3707cdc463b825bd17268cdea
xaxage <xaxage@gmail.com>
2022-01-21 01:56:52 +0400

    Add PetTypeService. Closes #30

---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.xaxage.xaxagepetclinic.services;
+
+
+import com.xaxage.xaxagepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.xaxage.xaxagepetclinic.services.map;
+
+import com.xaxage.xaxagepetclinic.model.PetType;
+import com.xaxage.xaxagepetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+}
