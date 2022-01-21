5a140188ff28bcbdd2f8b85080cb38658820bf71
xaxage <xaxage@gmail.com>
2022-01-21 08:05:41 +0400

    Add specialties map implementation. Closes #32

---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.xaxage.xaxagepetclinic.services;
+
+import com.xaxage.xaxagepetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package com.xaxage.xaxagepetclinic.services.map;
+
+import com.xaxage.xaxagepetclinic.model.Specialty;
+import com.xaxage.xaxagepetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
