4ecba5fe18c88825708c94df9f466ac401fbd600
xaxage <xaxage@gmail.com>
2022-01-26 04:33:44 -0800

    Add map impl for visit service. Closes #47

---
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.xaxage.xaxagepetclinic.services.map;
+
+import com.xaxage.xaxagepetclinic.model.Visit;
+import com.xaxage.xaxagepetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
