c8d11f507161da565d034434bff1db49b3f940f5
xaxage <xaxage@gmail.com>
2022-01-26 23:15:02 -0800

    Add project Lombok. Closes #52

--- pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/xaxage/xaxagepetclinic/model/BaseEntity.java
@@ -1,22 +1,25 @@
 package com.xaxage.xaxagepetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass//Indicates that this shouldn't have a table in a database
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
